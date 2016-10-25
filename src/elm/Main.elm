aa9dbb5f85ff57d73a0fc5f56106a93b6d871d4e
Maor Leger <maor.leger@gmail.com>
2016-10-25 13:34:52 -0600

    simple rendering of a board with pegs

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,90 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Html.Events exposing (onClick)
+import String exposing (toLower)
 
 
 -- APP
+
+
 main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+type Peg
+    = Orange
+    | Yellow
+    | Green
+    | Red
+    | Blue
+    | Pink
+
+
+type alias Score =
+    ( Int, Int )
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Round =
+    { guess : List Peg
+    , score : Score
+    }
+
+
+type alias Model =
+    List Round
+
+
+model : Model
+model =
+    [ (Round [ Pink, Yellow, Blue, Green ] ( 0, 1 ))
+    , (Round [ Blue, Blue, Blue, Blue ] ( 0, 0 ))
+    ]
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | Guess (List Peg)
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        Guess pegs ->
+            model
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    ol [ class "board" ] <| List.map round model
+
+
+round : Round -> Html Msg
+round { guess, score } =
+    li [ class "round" ]
+        [ span [ class "pegs" ] <| List.map pegRenderer guess
+        , span [ class "score" ] [ text ("score") ]
         ]
-      ]
-    ]
-  ]
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+pegRenderer : Peg -> Html Msg
+pegRenderer peg =
+    let
+        pegColor =
+            "round__peg--" ++ (toLower <| toString peg)
+    in
+        span [ class <| "round__peg " ++ pegColor ] []
